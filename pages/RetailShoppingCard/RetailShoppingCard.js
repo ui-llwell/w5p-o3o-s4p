fef11ef977b5d830b768c40599c68771bf6ed067
Una <una_bella@qq.com>
2018-10-15 15:51:13 +0800

    【UI】零售购物清单 、o2o购物车编辑及删除
    closes # 4

--- pages/RetailShoppingCard/RetailShoppingCard.js
+++ pages/RetailShoppingCard/RetailShoppingCard.js
@@ -5,7 +5,73 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    
+    shoppingCardData:{
+      title:"首都机场零售店",
+      ShoppingList: [
+        {
+        goodsId: '1',
+        goodsName: '特别长特别长特别长特别长特别长的耳机',
+        goodsImg: 'http://img.ui.cn/data/file/7/7/6/992677.png',
+        goodsPrice: 1,
+        goodsNum: 1,
+      }, {
+          goodsId: '2',
+          goodsName: '特别长特别长特别长特别长特别长的耳机',
+          goodsImg: 'http://img.ui.cn/data/file/7/7/6/992677.png',
+          goodsPrice: 2,
+          goodsNum: 2,
+        }, {
+          goodsId: '3',
+          goodsName: '特别长特别长特别长特别长特别长的耳机',
+          goodsImg: 'http://img.ui.cn/data/file/7/7/6/992677.png',
+          goodsPrice: 3,
+          goodsNum: 3,
+        }, {
+          goodsId: '4',
+          goodsName: '特别长特别长特别长特别长特别长的耳机',
+          goodsImg: 'http://img.ui.cn/data/file/7/7/6/992677.png',
+          goodsPrice: 4,
+          goodsNum: 4,
+        }, {
+          goodsId: '5',
+          goodsName: '特别长特别长特别长特别长特别长的耳机',
+          goodsImg: 'http://img.ui.cn/data/file/7/7/6/992677.png',
+          goodsPrice: 5,
+          goodsNum: 5,
+        }, {
+          goodsId: '6',
+          goodsName: '特别长特别长特别长特别长特别长的耳机',
+          goodsImg: 'http://img.ui.cn/data/file/7/7/6/992677.png',
+          goodsPrice: 6,
+          goodsNum: 6,
+        }, {
+          goodsId: '7',
+          goodsName: '特别长特别长特别长特别长特别长的耳机',
+          goodsImg: 'http://img.ui.cn/data/file/7/7/6/992677.png',
+          goodsPrice: 7,
+          goodsNum: 7,
+        }, {
+          goodsId: '8',
+          goodsName: '特别长特别长特别长特别长特别长的耳机',
+          goodsImg: 'http://img.ui.cn/data/file/7/7/6/992677.png',
+          goodsPrice: 8,
+          goodsNum: 8,
+        }, {
+          goodsId: '9',
+          goodsName: '特别长特别长特别长特别长特别长的耳机',
+          goodsImg: 'http://img.ui.cn/data/file/7/7/6/992677.png',
+          goodsPrice: 9,
+          goodsNum: 9,
+        }, {
+          goodsId: '10',
+          goodsName: '特别长特别长特别长特别长特别长的耳机',
+          goodsImg: 'http://img.ui.cn/data/file/7/7/6/992677.png',
+          goodsPrice: 10,
+          goodsNum: 10,
+        }
+        ]
+    }
   },
 
   /**
@@ -28,7 +94,35 @@ Page({
   onShow: function () {
 
   },
-
+  // =====================================================
+  sss:function(e){
+    e.prevent;
+    console.log('2222', e.currentTarget.dataset.index)
+    let curIndex = e.currentTarget.dataset.index
+    this.data.shoppingCardData.ShoppingList.splice(curIndex, 1);
+    this.setData({
+      'shoppingCardData.ShoppingList': this.data.shoppingCardData.ShoppingList,
+    })
+  },
+  gotoGoodsDetails: function () {
+    console.log('跳页gotoGoodsDetails')
+  },
+  // 防止 点击编辑数量时 跳转事件
+  prevent: function (e) {
+    e.prevent;
+  },
+  // 修改数量
+  addNumber(e) {
+    console.log('ww', e);
+    let curNum = 'shoppingCardData.ShoppingList[' + e.currentTarget.dataset.index + '].goodsNum';
+    this.setData({
+      [curNum]: e.detail.value,
+    })
+  },
+  // 提交付款
+  gotoRetailOrderConfirm: function () {
+    console.log('跳页')
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
